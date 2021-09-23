417e28f5afe43cd10410aac1c0343aef10203f70
ZeeTom <zthom13@hotmail.com>
2021-09-23 17:54:37 -0600

    added profile form bootstrap

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -3,14 +3,14 @@ import Alert from "./Alert";
 import "./Profile.css";
 
 /** Profile: renders form to update user information
- *    props: 
+ *    props:
  *      - currentUser: { username, firstName, lastName, email, isAdmin, applications }
  *      - updateUser: parent function
- * 
+ *
  *    states:
  *      - formData: { username, firstName, lastName, email }
  *      - errors: ["error message", ...]
- * 
+ *
  *    Routes -> Profile
  */
 function Profile({ currentUser, updateUser }) {
@@ -18,7 +18,7 @@ function Profile({ currentUser, updateUser }) {
     username: currentUser.username,
     firstName: currentUser.firstName,
     lastName: currentUser.lastName,
-    email: currentUser.email
+    email: currentUser.email,
   });
   const [errors, setErrors] = useState([]);
 
@@ -46,42 +46,67 @@ function Profile({ currentUser, updateUser }) {
   return (
     <div className="Profile">
       <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
-        <input
-          readOnly={true}
-          id="username"
-          name="username"
-          value={formData.username}
-        ></input>
-        <label htmlFor="firstName">First Name</label>
-        <input
-          onChange={handleChange}
-          id="firstName"
-          name="firstName"
-          value={formData.firstName}
-        ></input>
-        <label htmlFor="last-name">Last Name</label>
-        <input
-          onChange={handleChange}
-          id="lastName"
-          name="lastName"
-          value={formData.lastName}
-        ></input>
-        <label htmlFor="email">Email</label>
-        <input
-          onChange={handleChange}
-          id="email"
-          name="email"
-          value={formData.email}
-        ></input>
-        <label htmlFor="password">Confirm password to make changes</label>
-        <input
-          onChange={handleChange}
-          id="password"
-          name="password"
-          type="password"
-          autoComplete="off"
-        ></input>
+        <div className="form-row col-6 col-lg-4 mx-auto mb-1">
+          <label className="col-form-label" htmlFor="username">
+            Username
+          </label>
+          <input
+            className="form-control"
+            readOnly={true}
+            id="username"
+            name="username"
+            value={formData.username}
+          ></input>
+        </div>
+        <div className="form-row col-6 col-lg-4 mx-auto mb-1">
+          <label className="col-form-label" htmlFor="firstName">
+            First Name
+          </label>
+          <input
+            className="form-control"
+            onChange={handleChange}
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+          ></input>
+        </div>
+        <div class="form-row col-6 col-lg-4 mx-auto mb-1">
+          <label className="col-form-label" htmlFor="last-name">
+            Last Name
+          </label>
+          <input
+            className="form-control"
+            onChange={handleChange}
+            id="lastName"
+            name="lastName"
+            value={formData.lastName}
+          ></input>
+        </div>
+        <div className="form-row col-6 col-lg-4 mx-auto mb-1">
+          <label className="col-form-label" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="form-control"
+            onChange={handleChange}
+            id="email"
+            name="email"
+            value={formData.email}
+          ></input>
+        </div>
+        <div className="form-row col-6 col-lg-4 mx-auto mb-1 mt-3">
+          <label className="col-form-label" htmlFor="password">
+            Confirm password to make changes
+          </label>
+          <input
+            className="form-control"
+            onChange={handleChange}
+            id="password"
+            name="password"
+            type="password"
+            autoComplete="off"
+          ></input>
+        </div>
         {errors.length > 0
           ? errors.map((error, i) => <Alert error={error} key={i} />)
           : ""}
